99446ee26a9792643e2958d0cd1f7d8d1583c81f
Michael Shipman <mls.shipman@gmail.com>
2014-04-27 13:28:08 -0400

    Peer Review Code
    This is the branch that has the completed cachematrix functions solved
    for the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##  OK, Here goes...
+##This function will create a "special function" list that is called upon by the
+##  cacheSolve function.  Each element of the resulting list will create conditions
+##  needed to either create or recall a cached inverted matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        #Determines the matrix dimensions.
+        Dims <- dim(X)
+        x <- Dims[1]
+        y <- Dims[2]
+        
+        # Tests if the input matrix is square.  If not, a message is returned and function
+        # will end. "Bullet-Proofs" the code so that only a square matrix can be used.
+        if (x != y) {
+                message("matrix is not square.")
+                stop("Must use a square, non-singular input matrix.") 
+        }
+        
+        #Creates an empty matrix with dimensions similar to that of the input matrix.
+        SqMat <- matrix(,x,y)
+        
+        #Sets up the "special matrix" list needed for the cachesolve function.
+        setMat <- function(Y) {
+                X <<- Y
+                SqMat <<- matrix()
+        }
+        getMat <- function() X
+        setInverse <- function(solve) SqMat <<- Inverse
+        getInverse <- function() SqMat
+        list(setMat = setMat, getMat = getMat,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function will return the inverted matrix using the "special matrix" list
+##  output from "makeCacheMatrix" function.  If the inverted matrix does not previously
+##  exist, then the function will use the built-in "solve" function to invert the matrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #Gets the stored square matrix that is inverted.
+        SqMat <- X$getInverse()
+        #Tests to determine if the stored matrix is empty or not empty.  If not empty,
+        #  then it retreives the inverted matrix from the cache.
+        if(!is.na(SqMat[1,1])) {
+                message("getting cached data")
+                return(SqMat)
+        }
+        #The rest of this code will invert the matrix, since the if-then condition above for
+        # a stored inverted matrix does not exist yet, then returns the inverted matrix.
+        MatData <- X$getMat()
+        SqMat <- solve(MatData, ...)
+        X$setMat(SqMat)
+        SqMat
 }
